efd1a82977166452d752815689dcb3e1ac1dbec9
adiii013 <adidada023@gmail.com>
2023-03-19 18:56:18 +0530

    aaded

--- index.js
+++ index.js
@@ -1,120 +1,56 @@
-function popFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+let pc = 0, player = 0;
+window.onload = function () {
+    if (sessionStorage.getItem('pc')) {
+        pc = sessionStorage.getItem('pc');
+        player = sessionStorage.getItem('player');
+    }
+    else {
+        sessionStorage.setItem('pc', 0)
+        sessionStorage.setItem('player', 0);
+        sessionStorage.setItem('pcResult','demo')
+        sessionStorage.setItem('playerResult','demo')
+    }
+    document.querySelector('.Cscore').innerHTML = pc;
+    document.querySelector('.Yscore').innerHTML = player;
 }
 
-const scissorChild = `<div class="scissor__container circle scissor">
-<img src="./assets/scissor.png" alt="" srcset="">
-</div>`;
 
-const rockChild = `<div class="rock__container circle rock"><img src="./assets/rock.png" alt="">
-</div>`;
+function hideRules(){
+    document.querySelector('.rules__img').style.visibility = 'hidden';
+  }
+  
+  function showRules(){
+    document.querySelector('.rules__img').style.visibility = 'visible';
+  }
 
-const paperChild = `<div class="paper__container circle paper">
-<img src="./assets/hand.png" alt="">
-</div>`;
+const setResult = (player,pc)=>{
+    sessionStorage.setItem('pcResult',pc);
+    sessionStorage.setItem('playerResult',player);
+    window.location.href = 'result.html'
+}
 
-const game = () => {
-  let playerScore = 0;
-  document.querySelector(".result__container").style.visibility = "hidden";
-  document.querySelector(".result__container").style.position = "absolute";
 
-  // Function to
-  const playGame = () => {
+const playGame = () => {
     const rockBtn = document.querySelector(".rock");
     const paperBtn = document.querySelector(".paper");
-    const scissorBtn = document.querySelector(".scissor");
+    const scissorBtn = document.querySelector(".scissors");
     const playerOptions = [rockBtn, paperBtn, scissorBtn];
     const computerOptions = ["rock", "paper", "scissors"];
     playerOptions.forEach((option) => {
-      option.addEventListener("click", function () {
-        const choiceNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[choiceNumber];
-        if (option === rockBtn) {
-          winner("rock", computerChoice, option);
-        }
-        if (option === paperBtn) {
-          winner("paper", computerChoice, option);
-        }
-        if (option === scissorBtn) {
-          winner("scissors", computerChoice, option);
-        }
-      });
+        option.addEventListener("click", function () {
+            const choiceNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOptions[choiceNumber];
+            if (option === rockBtn) {
+                setResult("rock", computerChoice);
+            }
+            if (option === paperBtn) {
+                setResult("paper", computerChoice);
+            }
+            if (option === scissorBtn) {
+                setResult("scissors", computerChoice);
+            }
+        });
     });
-  };
-
-  const winner = (player, computer, playerOption) => {
-    document.querySelector(".component__container").style.visibility = "hidden";
-    document.querySelector(".component__container").style.position = "absolute";
-    document.querySelector(".result__container").style.visibility = "visible";
-    document.querySelector(".result__container").style.position = "static";
-    const playerScoreBoard = document.querySelector(".score");
-    const result = document.querySelector(".result");
-    const against = document.querySelector(".against");
-    const playerResult = document.querySelector(".player__result");
-    const pcResult = document.querySelector(".pc__result");
-    player = player.toLowerCase();
-    computer = computer.toLowerCase();
-
-    if(player === 'rock') {
-		playerResult.innerHTML = rockChild;
-	}else if(player === 'paper') {
-		playerResult.innerHTML = paperChild;
-	}else{
-        playerResult.innerHTML = scissorChild;
-	}
-
-	if(computer === 'rock') {
-		pcResult.innerHTML = rockChild;
-	}else if(computer === 'paper') {
-		pcResult.innerHTML = paperChild;
-	}else{
-        pcResult.innerHTML = scissorChild;
-	}
-
-    if (player == computer) {
-      result.innerHTML = "Tie";
-      against.innerHTML = "";
-    } else if (player === "rock") {
-      if (computer === "paper") {
-        result.textContent = "PC Won";
-		against.textContent = "Against You"
-      } else {
-        result.textContent = "You Won";
-		against.textContent = "Against PC"
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;
-      }
-    } else if (player === "scissors") {
-      if (computer === "rock") {
-        result.textContent = "PC Won";
-		against.textContent = "Against You"
-      } else {
-        result.textContent = "You Won";
-		against.textContent = "Against PC"
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;
-      }
-    } else if (player === "paper") {
-      if (computer === "scissors") {
-        result.textContent = "PC Won";
-		against.textContent = "Against You"
-      } else {
-        result.textContent = "You Won";
-		against.textContent = "Against PC"
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;
-      }
-    }
-  };
-  playGame();
 };
 
-function playAgain() {
-  document.querySelector(".component__container").style.visibility = "visible";
-  document.querySelector(".component__container").style.position = "static";
-  document.querySelector(".result__container").style.visibility = "hidden";
-  document.querySelector(".result__container").style.position = "absolute";
-}
-
-game();
+playGame();
